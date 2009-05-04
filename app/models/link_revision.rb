64f4082bb3b72cc0ccfc4e61e3fa9bbb7f9d67c1
Rich Cavanaugh <rich.cavanaugh@tycoonresearch.com>
2009-05-04 16:31:54 -0400

    add a few comments clarifying the more complicated parts

--- app/models/link_revision.rb
+++ app/models/link_revision.rb
@@ -3,9 +3,17 @@ class LinkRevision < ActiveRecord::Base
   
   belongs_to :post, :class_name => "PostRevision"
   
+  # don't change our post association unless the post itself
+  # is in the middle of changing as well.
+  # 
+  # this could also have been done in Link using an after_revise
+  # callback. I'm doing it here using a normal rails callback because
+  # the revision is what I'm modifying so, it makes more sense to look
+  # for code changing the revisions here.
   before_create :reassociate_with_post, :if => :post_in_revision?
   
   private
+    # grab the newest post *revision* and associate ourselves with that
     def reassociate_with_post
       self.post = self.current_revision.post.find_revision(:previous)
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
   acts_as_revisable
   
+  # any time a link is added we should trigger a revision
   has_many :links, :before_add => :revise!
   
+  # after we've been revised let's trigger the revision of all our links
   after_revise :revise_links!
   
   private
